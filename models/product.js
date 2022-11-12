f923f44198534fa34b231a55a3760b452a7a5f65
roshinys <roshinchaya@gmail.com>
2022-11-12 11:42:33 +0530

    mongo basics

---
+++ models/product.js
@@ -0,0 +1,23 @@
+const getDB = require("../util/database").getDB;
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
+  }
+  save() {
+    const db = getDB();
+    return db
+      .collection("products")
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+module.exports = Product;
